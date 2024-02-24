b9d4d8786ee4601c6a33b5e0ea15bc434dfd835b
websitesdev <website_dev@yahoo.com>
2024-02-24 10:35:26 +0200

    root

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/index.scss';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(<App/>,document.getElementById("root"));
+
+//ReactDOM.render(<App/>,document.getElementById("root"));
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="root" />);
